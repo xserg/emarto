b5fe1fd3a431b65c0d5e49f6021315fa1345b44c
xserg <xxserg@gmail.com>
2024-06-25 13:06:55 +0300

    counter console

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,12 +1,10 @@
 
 function count_chars(input, output)
 {
-  console.log(input, output);
   $(input).on("input", function() {
       var maxlength = $(this).attr("maxlength");
       var currentLength = $(this).val().length;
       $(output).html(maxlength - currentLength);
 
-      console.log(maxlength - currentLength);
   });
 }
