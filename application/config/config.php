75fbc2c02512e1c7e8c7771fe06f124f4329de05
xserg <xxserg@gmail.com>
2025-08-18 12:10:18 +0300

    session ip

--- application/config/config.php
+++ application/config/config.php
@@ -378,11 +378,11 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 259200;//3 days
 $config['sess_save_path'] = 'ci_sessions';
-$config['sess_match_ip'] = FALSE;
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = TRUE;
 
@@ -454,7 +454,7 @@ $config['csrf_token_name'] = 'csrf_mds_token';
 $config['csrf_cookie_name'] = 'csrf_mds_token';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array('iyzico-payment-post', 'stripe-payment-post', 'mercado-pago-payment-post', 'mds-run-internal-cron');
+$config['csrf_exclude_uris'] = array('iyzico-payment-post', 'stripe-payment-post', 'mercado-pago-payment-post', 'mds-run-internal-cron', 'send-otp', 'verify-otp');
 
 /*
 |--------------------------------------------------------------------------
