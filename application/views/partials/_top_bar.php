5668ef5e0c0728a0e2a2ec71d73b6b47a1fa8967
xserg <xxserg@gmail.com>
2025-12-18 12:07:21 +0300

    sale icon

--- application/views/partials/_top_bar.php
+++ application/views/partials/_top_bar.php
@@ -107,7 +107,10 @@
                                 <?php if (is_vendor()): ?>
                                     <li>
                                         <a href="<?php echo dashboard_url()."sales"; ?>">
-                                            <i class="fa fa-edit"></i>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+                                                <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0"/>
+                                                <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1m0 5.586 7 7L13.586 9l-7-7H2z"/>
+                                                </svg>
                                             <?php echo trans("sales"); ?>
                                         </a>
                                     </li>
@@ -115,10 +118,7 @@
                                 <?php if ($this->is_sale_active): ?>
                                     <li>
                                         <a href="<?php echo generate_url("orders"); ?>">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
-                                            <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0"/>
-                                            <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1m0 5.586 7 7L13.586 9l-7-7H2z"/>
-                                            </svg>
+                                            <i class="icon-shopping-basket"></i>
                                             <?php echo trans("orders"); ?>
                                         </a>
                                     </li>
